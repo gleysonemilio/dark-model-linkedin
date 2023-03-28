5f1db5e456a8e1ca3ee24de806f591c216739a34
gleysonemilio <gleyson.emilio@outlook.com>
2023-03-28 11:42:21 -0300

    feat: icon mynwtworks

--- src/components/MyNetworks/index.tsx
+++ src/components/MyNetworks/index.tsx
@@ -1,27 +1,21 @@
-import { Github, Linkedin } from 'lucide-react'
 import './styles.css'
 
 export default function MyNetworks() {
   return (
     <div className="container-aside">
-      <span>my networks</span>
+      <p>my networks</p>
       <div className="container-links">
         <a href="https://www.linkedin.com/in/gleysonsilva/" target="_blank" rel="noreferrer">
-          <Linkedin size={20} />
-          {/* color={'#0a66c2'} */}
-          <span>Linkedin</span>
+          <i className="devicon-linkedin-plain " />
+          <span>LinkedIn</span>
         </a>
         •
-        <a
-          href="https://github.com/gleysonemilio/dask-model-linkedin"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Github size={20} />
+        <a href="https://github.com/gleysonemilio" target="_blank" rel="noreferrer">
+          <i className="devicon-github-original" />
           <span>Github</span>
         </a>
       </div>
-      <span>Developed by Gleyson Silva</span>
+      <p>Developed by Gleyson Silva</p>
     </div>
   )
 }
